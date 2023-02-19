c28341ffcb35a7c69a20c4745d22089bb5f60a84
Chris Whitlam <chris.j.whitlam@gmail.com>
2023-02-19 17:07:51 +0000

    Temproarily remove state rehydration

--- src/shared/store/store.js
+++ src/shared/store/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import planetReducer from './planetSlice';
 import sceneReducer from './sceneSlice';
 import controlsReducer from './controlsSlice';
-import { sessionStorageMiddleware, reHydrateStore } from './middleware';
+import { sessionStorageMiddleware } from './middleware';
 
 const store = configureStore({
   reducer: {
@@ -10,7 +10,7 @@ const store = configureStore({
     scene: sceneReducer,
     controls: controlsReducer
   },
-  preloadedState: reHydrateStore(),
+  // preloadedState: reHydrateStore(),
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(sessionStorageMiddleware),
   devTools: process.env.NODE_ENV !== 'production'
