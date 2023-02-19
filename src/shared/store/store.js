5f73df5212c91901eeffaa51aa0973f4b476b108
Chris Whitlam <chris.j.whitlam@gmail.com>
2023-02-19 18:15:57 +0000

    Tidy up code

--- src/shared/store/store.js
+++ src/shared/store/store.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import planetReducer from './planetSlice';
 import sceneReducer from './sceneSlice';
 import controlsReducer from './controlsSlice';
-import { sessionStorageMiddleware } from './middleware';
 
 const store = configureStore({
   reducer: {
@@ -10,9 +9,6 @@ const store = configureStore({
     scene: sceneReducer,
     controls: controlsReducer
   },
-  // preloadedState: reHydrateStore(),
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(sessionStorageMiddleware),
   devTools: process.env.NODE_ENV !== 'production'
 });
 
